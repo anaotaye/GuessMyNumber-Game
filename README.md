10ec3a0932977d8fd0339b215447c20a9327d935
Otaye <otayeanastasia@gmail.com>
2022-05-21 19:05:06 +0100

    Adding my project

---
+++ README.md
@@ -0,0 +1,11 @@
+This repository contains the HTML, CSS and JavaScript of a simple and functional guess-number game.
+
+The rules of the game are:
+1. A random number between 1 and 20 is generated in every new game.
+2. An input field is provided to input the guessed number.
+3. Click on the 'Check!' button to check if that's the correct number.
+4. If the number is too high or low, it will be displayed and if score is greater than 0, another opportunity is given to input another guessed number and check.
+5. If the score is equals to 0, you've lost and have to tap on 'Again!' to play again.
+6. If the guessed number is correct, the number will be displayed on the question mark box and it will be displayed that you won.
+7. Every game has 20 chances or scores to try.
+
---
+++ script.js
@@ -0,0 +1,69 @@
+// 'use strict';
+
+// console.log(displayMessage(// displayMessage('Correct Number!';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  //When there's no input
+  if (!guess) {
+    displayMessage('⛔ No number');
+  }
+
+  //When player wins
+  else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  }
+
+  //When gues is wrong
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📈 Too Low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+//Again button for reset
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // const guess = Number(document.querySelector('.guess').value);
+
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  document.querySelector('.number').style.width = '15rem';
+});
